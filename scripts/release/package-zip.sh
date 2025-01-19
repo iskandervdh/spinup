05ec76aa579998ac7131bb702d3def15249eda1d
Iskander <iskandervdh@gmail.com>
2025-01-19 12:09:33 +0100

    Add execution permission to release packaging scripts and try to match any zip file

--- scripts/release/package-zip.sh
+++ scripts/release/package-zip.sh
@@ -2,24 +2,24 @@
 
 SPINUP_VERSION=$(cat ./common/.version | sed 's/^v//')
 
-MAC_OS_FOLDER="./build/MacOS"
+MAC_OS_DIR="./build/macos"
 
 # Define the path to the binary file
 BIN_FILE="./build/bin/Spinup.app/Contents/MacOS/spinup"
 
-# Create a MacOS directory
-mkdir -p $MAC_OS_FOLDER
+# Create the mac os directory
+mkdir -p $MAC_OS_DIR
 
 # Copy the contents of the build/unix directory to the MacOS directory
-cp -r "./build/unix/." $MAC_OS_FOLDER
+cp -r "./build/unix/." $MAC_OS_DIR
 
 # Copy the binary file to the MacOS directory
-mkdir -p "$MAC_OS_FOLDER/usr/share/spinup/bin"
-cp $BIN_FILE "$MAC_OS_FOLDER/usr/share/spinup/bin"
+mkdir -p "$MAC_OS_DIR/usr/share/spinup/bin"
+cp $BIN_FILE "$MAC_OS_DIR/usr/share/spinup/bin"
 
 # Copy postinstall script to the MacOS directory
-cp "./build/DEBIAN/postinst" $MAC_OS_FOLDER
-mv "$MAC_OS_FOLDER/postinst" "$MAC_OS_FOLDER/post_install.sh"
+cp "./build/DEBIAN/postinst" $MAC_OS_DIR
+mv "$MAC_OS_DIR/postinst" "$MAC_OS_DIR/post_install.sh"
 
 # Create a zip file containing the contents of the MacOS directory
-(cd $MAC_OS_FOLDER && zip -r "../../spinup-${SPINUP_VERSION}-macos.zip" .)
+(cd $MAC_OS_DIR && zip -r "../../spinup-${SPINUP_VERSION}-macos.zip" .)
