d8990e3417015bf09e890c60d5c91b12103b1f3e
Iskander <iskandervdh@gmail.com>
2025-01-19 13:20:45 +0100

    Add comments to .deb build script

--- scripts/release/package-deb.sh
+++ scripts/release/package-deb.sh
@@ -3,21 +3,28 @@
 SPINUP_VERSION=$(cat ./common/.version | sed 's/^v//')
 
 for os_version in "" "-ubuntu24.04"; do
+    # Create the directory structure for the .deb package
     mkdir -p deb/spinup-${SPINUP_VERSION}${os_version}/DEBIAN
     mkdir -p deb/spinup-${SPINUP_VERSION}${os_version}/usr/share/spinup/bin
+
+    # Copy the necessary files to the .deb package directory
     cp build/bin/spinup-${SPINUP_VERSION}${os_version} deb/spinup-${SPINUP_VERSION}${os_version}/usr/share/spinup/bin/spinup
     cp build/DEBIAN/* deb/spinup-${SPINUP_VERSION}${os_version}/DEBIAN
     cp -r build/unix/usr deb/spinup-${SPINUP_VERSION}${os_version}
 
+    # Update the control file with the current version number
     echo -e "\nVersion: $SPINUP_VERSION" >> deb/spinup-${SPINUP_VERSION}${os_version}/DEBIAN/control
 
-    if [ "$webkit_version" -eq "40" ]; then
+    # Add the dependencies for the .deb package
+    if [ "$os_version" -eq "" ]; then
         echo "Depends: dnsmasq, libgtk-3-0, libwebkit2gtk-4.0-dev" >> deb/spinup-${SPINUP_VERSION}${os_version}/DEBIAN/control
     else
         echo "Depends: dnsmasq, libgtk-3-0, libwebkit2gtk-4.1-dev" >> deb/spinup-${SPINUP_VERSION}${os_version}/DEBIAN/control
     fi
 
+    # Set the permissions for the .deb package files
     sudo chown -R root:root deb/spinup-${SPINUP_VERSION}${os_version}
 
+    # Build the .deb package
     dpkg-deb --build deb/spinup-${SPINUP_VERSION}${os_version}
 done
