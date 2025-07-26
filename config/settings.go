696c50f50be34a2395a0c67d07a7ff88a4e3ebc6
Iskander <iskandervdh@gmail.com>
2025-07-26 13:45:35 +0200

    Add settings management functions for reading and writing configuration

---
+++ config/settings.go
@@ -0,0 +1,75 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path"
+)
+
+func (c *Config) writeSettings(settings map[string]interface{}) error {
+	settingFilePath := path.Join(c.configDir, "settings.json")
+
+	data, err := json.MarshalIndent(settings, "", "  ")
+
+	if err != nil {
+		return fmt.Errorf("could not marshal settings: %w", err)
+	}
+
+	err = os.WriteFile(settingFilePath, data, 0644)
+
+	if err != nil {
+		return fmt.Errorf("could not write settings file: %w", err)
+	}
+
+	return nil
+}
+
+func (c *Config) GetSettings() (map[string]interface{}, error) {
+	settingFilePath := path.Join(c.configDir, "settings.json")
+	settings := make(map[string]interface{})
+
+	data, err := os.ReadFile(settingFilePath)
+
+	if err != nil {
+		return nil, fmt.Errorf("could not read settings file: %w", err)
+	}
+
+	err = json.Unmarshal(data, &settings)
+
+	if err != nil {
+		return nil, fmt.Errorf("could not unmarshal settings: %w", err)
+	}
+
+	return settings, nil
+}
+
+func (c *Config) GetSetting(settingKey string) (interface{}, error) {
+	settings, err := c.GetSettings()
+	if err != nil {
+		return nil, fmt.Errorf("could not read settings: %w", err)
+	}
+
+	value, exists := settings[settingKey]
+	if !exists {
+		return nil, fmt.Errorf("setting %s does not exist", settingKey)
+	}
+
+	return value, nil
+}
+
+func (c *Config) SetSetting(settingKey string, value interface{}) error {
+	settings, err := c.GetSettings()
+
+	if err != nil {
+		return fmt.Errorf("could not read settings: %w", err)
+	}
+
+	settings[settingKey] = value
+	err = c.writeSettings(settings)
+
+	if err != nil {
+		return fmt.Errorf("could not write settings: %w", err)
+	}
+	return nil
+}
