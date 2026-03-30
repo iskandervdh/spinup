84be253e915c91c7ea2dec9713c1bb632d297ff9
Iskander <iskandervdh@gmail.com>
2026-03-30 12:22:37 +0200

    Upgrade Tailwind CSS to version 4

--- frontend/src/components/checkbox.tsx
+++ frontend/src/components/checkbox.tsx
@@ -9,7 +9,7 @@ export function Checkbox({
     <input
       type="checkbox"
       className={cn(
-        'w-5 h-5 transition-colors duration-200 ease-in-out border cursor-pointer rounded-md appearance-none outline-offset-2 focus-visible:outline outline-1 outline-primary bg-background text-white border-primary grid place-content-center before:invisible before:checked:visible before:w-3 before:h-3 before:rounded-sm before hover:outline before:bg-primary checked:border-primary',
+        'w-5 h-5 transition-colors duration-200 ease-in-out border cursor-pointer rounded-md appearance-none outline-offset-2 focus-visible:outline-solid outline-1 outline-primary bg-background text-white border-primary grid place-content-center before:invisible checked:before:visible before:w-3 before:h-3 before:rounded-xs before hover:outline-solid before:bg-primary checked:border-primary',
         className
       )}
       {...props}
